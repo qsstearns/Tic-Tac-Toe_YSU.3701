9ad6f8953a6b8fbee45da70fce93073777b88334
Quintin Stearns <quintinstearns@yahoo.com>
2019-10-22 19:26:02 -0400

    Auto stash before revert of "Alexanders Changes"

--- FinalProject/src/finalproject/TicTacToe.java
+++ FinalProject/src/finalproject/TicTacToe.java
@@ -11,20 +11,29 @@ package finalproject;
  */
 public class TicTacToe {
     
+   protected char[][] pieces = new char[3][3];
     public TicTacToe(){
-        
+        //pieces[1][1] = 'O';
+        //pieces[2][2] = 'X';
     }
     
     
     public void initializeBoard(){
         
         for(int r = 0; r < 3; r++){
-            System.out.println("\n---------");
+            System.out.println("\n-------");
             
             for(int c = 0; c < 3; c++){
-                System.out.print(" | ");
+                System.out.print("| ");
+                
+                if(pieces[r][c] == 'X'){
+                    System.out.print('X');
+                }
+                else if(pieces[r][c] == 'O'){
+                    System.out.print('O');
+                }
             }
         }
-        System.out.println("\n---------");
+        System.out.println("\n-------");
     }
 }
